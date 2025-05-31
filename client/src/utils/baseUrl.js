297a3f194632aa004b6b284fd68d1d9521f40303
manjitsyngh <manjitsingh13septem@gmail.com>
2025-05-31 16:23:27 +0530

    url

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 
 
-export const baseUrl = `http://localhost:3000`;
+// export const baseUrl = `http://localhost:3000`;
 // export const baseUrl = `https://get-independent.onrender.com`;
-// export const baseUrl = `https://skilzyy.onrender.com`;
+export const baseUrl = `https://skilzyy.onrender.com`;
