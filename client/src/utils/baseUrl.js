7be0fbb27292985b85e4547f96ac97fed68e34c4
manjitsyngh <manjitsingh13septem@gmail.com>
2025-06-10 09:18:11 +0530

    url

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 
 
-export const baseUrl = `http://localhost:3000`;
+// export const baseUrl = `http://localhost:3000`;
 // export const baseUrl = `https://get-independent.onrender.com`;
 // export const baseUrl = `https://skilzyy.onrender.com`;
-// export const baseUrl = `https://skilzyy-kohl.vercel.app`;
+export const baseUrl = `https://skilzyy-kohl.vercel.app`;
