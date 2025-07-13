131a7623f688a0e6177c844b188d0fa19d7b50e6
itstijnam <manjitsyngh@gmail.com>
2025-07-13 22:01:03 +0530

    link

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -3,4 +3,5 @@
 // export const baseUrl = `http://localhost:3000`;
 // export const baseUrl = `https://get-independent.onrender.com`;
 // export const baseUrl = `https://skilzyy.onrender.com`;
-export const baseUrl = `https://skilzyy-kohl.vercel.app`;
+// export const baseUrl = `https://skilzyy-kohl.vercel.app`;
+export const baseUrl = `https://skilzyy-vert.vercel.app`;
